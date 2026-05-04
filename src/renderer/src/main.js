09542e549291edbf46a304ad5347dcf62abcaeda
rvdstarre <info@starrevisie.nl>
2026-05-04 11:15:20 +0200

    Sentry: overstap naar @sentry/vue direct, consoleLoggingIntegration

--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -1,23 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
-import * as Sentry from '@sentry/capacitor'
-import { vueIntegration } from '@sentry/vue'
-import { consoleLoggingIntegration } from '@sentry/core'
+import * as Sentry from '@sentry/vue'
 
 const app = createApp(App)
 
 Sentry.init({
+  app,
   dsn: 'https://815b9bebeb5a505c53e0029a9580fae3@o4511330516664320.ingest.de.sentry.io/4511330529247312',
   release: 'ritme-timer@0.4.0',
   environment: import.meta.env.MODE,
   enableLogs: true,
   integrations: [
-    vueIntegration({ app }),
     Sentry.browserTracingIntegration(),
-    consoleLoggingIntegration()
+    Sentry.consoleLoggingIntegration()
   ],
-  tracesSampleRate: 0.2
+  tracesSampleRate: 1.0
 })
 
 app.mount('#app')
