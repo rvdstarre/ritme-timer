8cba82a71fdd2e14a1830e679e7c88e02093d973
rvdstarre <info@starrevisie.nl>
2026-05-04 11:00:24 +0200

    Sentry: enableLogs + consoleLoggingIntegration toegevoegd

--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './style.css'
 import * as Sentry from '@sentry/capacitor'
 import { vueIntegration } from '@sentry/vue'
+import { consoleLoggingIntegration } from '@sentry/core'
 
 const app = createApp(App)
 
@@ -10,9 +11,11 @@ Sentry.init({
   dsn: 'https://815b9bebeb5a505c53e0029a9580fae3@o4511330516664320.ingest.de.sentry.io/4511330529247312',
   release: 'ritme-timer@0.4.0',
   environment: import.meta.env.MODE,
+  enableLogs: true,
   integrations: [
     vueIntegration({ app }),
-    Sentry.browserTracingIntegration()
+    Sentry.browserTracingIntegration(),
+    consoleLoggingIntegration()
   ],
   tracesSampleRate: 0.2
 })
